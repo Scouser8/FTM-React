c67bec82f9c4942add51938276af9ea89102b77a
unknown <mohammed.abdelbaki96@hotmail.com>
2023-12-25 20:31:09 +0300

    Enhance App looking temporarily & adding template for login using Material UI/Installing MUI & @hookform/resolvers to integrate React hook form with Yup

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,9 +1,26 @@
+import { Box, Container, Typography } from "@mui/material";
+
 function Footer() {
   return (
-    <div>
-      Senior Frontend Developer Task - Giza Systems -
-      mohammed.abdelbaki96@hotmail.com © 2023
-    </div>
+    <Box
+      component="footer"
+      sx={{
+        py: 0.5,
+        mt: "auto",
+        backgroundColor: (theme) =>
+          theme.palette.mode === "light"
+            ? theme.palette.grey[200]
+            : theme.palette.grey[800],
+      }}
+    >
+      <Container sx={{ display: "flex", justifyContent: "center" }}>
+        <Typography variant="body1">
+          Senior Frontend Developer Task (Giza Systems) -
+          mohammed.abdelbaki96@hotmail.com © 2023
+        </Typography>
+        {/* <Copyright /> */}
+      </Container>
+    </Box>
   );
 }
 
